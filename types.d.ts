fb317a1d97435467d0f03e4b869ee60f1ab7d228
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-03-12 00:50:46 -0400

    updates

---
+++ types.d.ts
@@ -0,0 +1,14 @@
+// src/types.d.ts
+
+// config.ts
+export interface TokenMetadata {
+    mint: PublicKey;
+    name: string;
+    symbol: string;
+    image: string;
+    decimals: number;
+    baseWager: number;
+    poolAuthority?: PublicKey;
+    usdPrice?: number;
+  }
+  
